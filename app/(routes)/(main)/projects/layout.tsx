6a7a9aff994b78adaee31150278dffbb2a2818df
Waveless <itzwaveless@gmail.com>
2025-06-17 23:17:28 +0530

    fixed padding

--- app/(routes)/(main)/projects/layout.tsx
+++ app/(routes)/(main)/projects/layout.tsx
@@ -6,7 +6,9 @@ import { NextPage } from "next";
 const ProjectLayout: NextPage<PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen w-full">
-      <main className="flex-1 flex justify-center w-full">{children}</main>
+      <main className="flex-1 flex justify-center w-full pb-20">
+        {children}
+      </main>
 
       <Footer />
     </div>
