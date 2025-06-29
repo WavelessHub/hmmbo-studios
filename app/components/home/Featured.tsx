7f2e461820cb79560df85990ac6fa1375ce23579
Waveless <itzwaveless@gmail.com>
2025-06-29 12:48:15 +0530

    Fixed Padding in Home Screen

--- app/components/home/Featured.tsx
+++ app/components/home/Featured.tsx
@@ -1,36 +1,27 @@
 import { plugins } from "@/constants/plugins";
 
-
 import FeaturedCard from "./FeaturedCard";
 
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 
 const Featured = () => {
-
-
-  
   const [sliderRef] = useKeenSlider({
     loop: true,
     mode: "free-snap",
     slides: {
       perView: 3,
       spacing: 15,
-
     },
   });
 
   return (
     <div className="w-[80vw]">
-      <p className="text-5xl mb-10 font-semibold text-center text-secondary-gradient">
-      Our Best Picks
+      <p className="text-5xl my-20 font-semibold text-center text-secondary-gradient">
+        Our Best Picks
       </p>
 
-      <div
-        ref={sliderRef}
-        className="keen-slider "
-        
-      >
+      <div ref={sliderRef} className="keen-slider ">
         {plugins.map((plugin, index) => (
           <div className="keen-slider__slide" key={index}>
             <FeaturedCard {...plugin} />
--- app/components/home/Popularity.tsx
+++ app/components/home/Popularity.tsx
@@ -9,43 +9,34 @@ import { NextPage } from "next";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 
-
 const Popularity: NextPage = ({}) => {
-
-    const [sliderRef] = useKeenSlider({
-      loop: true,
-      mode: "free-snap",
-      slides: {
-        perView: 3,
-        spacing: 15,
-  
-      },
-      
-    });
+  const [sliderRef] = useKeenSlider({
+    loop: true,
+    mode: "free-snap",
+    slides: {
+      perView: 3,
+      spacing: 15,
+    },
+  });
 
   return (
     <div className="relative mt-96 pt-20 ">
       {/* Background section */}
       <div className="absolute left-0 top-[-15em] w-full h-full flex items-center justify-center ">
         <div>
-          <p className="text-5xl mt-20  h-20 font-semibold text-center text-secondary-gradient">
-          Hot Right Now  
+          <p className="text-5xl my-20 h-20 font-semibold text-center text-secondary-gradient">
+            Hot Right Now
           </p>
-      <div className="w-[80vw]">
-          <div
-        ref={sliderRef}
-        className="keen-slider "
-        
-      >
-            {popularities.map((plugin, index) => (
-               <div className="keen-slider__slide" key={index}>
-              <DisplayCard  {...plugin} popularTab />
-              </div>
-
-            ))}
+          <div className="w-[80vw]">
+            <div ref={sliderRef} className="keen-slider ">
+              {popularities.map((plugin, index) => (
+                <div className="keen-slider__slide" key={index}>
+                  <DisplayCard {...plugin} popularTab />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
       </div>
       <div className="bg-[#1A1919] w-full h-[24rem]" />
     </div>
